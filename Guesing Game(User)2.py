af87484b46858facb2d24693f01a0a94c4249af7
aniketsarkar100 <101813670+aniketsarkar100@users.noreply.github.com>
2024-12-01 20:00:57 +0530

    Update Guesing Game(User)2.py

--- Guesing Game(User)2.py
+++ Guesing Game(User)2.py
@@ -1,15 +1,3 @@
-# import random
-# def guess(x):
-#     random_number=random.randint(1,x)
-#     guess=0
-#     while guess!=random_number:
-#         guess=int(input(f'Guess a number between 1 & {x}: '))
-#         if guess<random_number:
-#             print("Sorry Try Again. Too Low.")
-#         elif guess>random_number:
-#             print("Sorry Try Again.Too High.")
-#     print(f"Yay you have guessed the number {random_number} correctly!!")
-# guess(10)
 import random
 def computer_guess(x):
     low=1
